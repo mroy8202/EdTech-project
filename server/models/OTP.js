b727cfa1d8e1b19430fbe726e6c199dbbff5ab21
mritunjayRoy <mroy8202@gmail.com>
2023-12-18 19:29:53 +0530

    mail sender function added

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -16,4 +16,22 @@ const otpSchema = new mongoose.Schema({
     }
 });
 
+// function -> to send mails
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
+        console.log("Email sent successfully: ", mailResponse);
+    }
+    catch(error) {
+        console.log("error occured while sending mail: ", error);
+        throw error;
+    }
+}
+
+// pre middleware
+otpSchema.pre("save", async function(next) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+}) 
+
 module.exports = mongoose.model("OTP", otpSchema);
