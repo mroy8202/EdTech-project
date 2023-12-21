03847308999d88e5215dff473e4fc1fb39baac3d
mritunjayRoy <mroy8202@gmail.com>
2023-12-21 11:57:34 +0530

    updateSection controller added

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -45,3 +45,35 @@ exports.createSection = async (req, res) => {
         });
     }
 }
+
+// updateSection -> handler
+exports.updateSection = async (req, res) => {
+    try {
+        // fetch data
+        const { sectionName, sectionId } = req.body;
+
+        // data validation
+        if(!sectionName || !sectionId) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required in section"
+            });
+        }
+
+        // update data
+        const section = await Section.findByIdAndUpdate(sectionId, {sectionName}, {new: true});
+
+        // return response
+        res.status(200).json({
+            success: true,
+            message: "section updated successfully",
+        });
+    }
+    catch(error) {
+        return res.status(500).json({
+            success: false,
+            message: "cannot update section details",
+            error: error.message
+        });
+    }
+}
