f621b1144e06cec8bdcaa7ef1f34d1eed0b52afc
mritunjayRoy <mroy8202@gmail.com>
2023-12-22 17:59:15 +0530

    Payments router added

---
+++ server/routes/Payments.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router()
+
+// import controllers and middlewares
+const { capturePayment, verifySignature } = require("../controllers/Payments")
+const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth");
+
+// routes
+router.post("/capturePayment", auth, isStudent, capturePayment);
+router.post("/verifySignature", verifySignature);
+
+// export router
+module.exports = router;
