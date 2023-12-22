4321390aaaf952a7e588941cbb9ff1a56b36ae4e
mritunjayRoy <mroy8202@gmail.com>
2023-12-22 17:32:23 +0530

    updateDisplayPicture handler added

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -91,3 +91,40 @@ exports.getAllUserDetails = async (req, res) => {
 		});
 	}
 };
+
+// updateDisplayPicture -> handler
+exports.updateDisplayPicture = async (req, res) => {
+    try {
+    	const displayPicture = req.files.displayPicture;
+      	const userId = req.user.id;
+		
+		// upload image to cloudinary
+		const image = await uploadImageToCloudinary(
+			displayPicture,
+			process.env.FOLDER_NAME,
+			1000,
+			1000
+		);
+		console.log(image);
+		
+		// update profile
+		const updatedProfile = await User.findByIdAndUpdate(
+			{ _id: userId },
+			{ image: image.secure_url },
+			{ new: true }
+		);
+		
+		// send successfull response
+		res.send({
+			success: true,
+			message: `Image Updated successfully`,
+			data: updatedProfile,
+		});
+    } 
+	catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+};
