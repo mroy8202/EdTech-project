88ea119f50392cb9bdb40ee4ec0da75de5afe7ef
mritunjayRoy <mroy8202@gmail.com>
2023-12-22 18:12:11 +0530

    Course router added

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -1,5 +1,5 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // import controllers and middlewares
 const { capturePayment, verifySignature } = require("../controllers/Payments")
