b0de0d2d3ac23a7d0f6846b19a5cdd352d276c85
mritunjayRoy <mroy8202@gmail.com>
2023-12-27 23:02:30 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar";
+import OpenRoute from "./components/core/Auth/OpenRoute";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -9,6 +12,18 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={
+          <OpenRoute>
+              <Signup />
+          </OpenRoute>
+        }/>
+        <Route path="/login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
       </Routes>
     </div>
   );
