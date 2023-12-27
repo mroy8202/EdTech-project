5bb8bd38b6c756845d8b6b2f7a268ddd70281144
mritunjayRoy <mroy8202@gmail.com>
2023-12-27 22:36:16 +0530

    Navbar modified

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, matchPath } from 'react-router-dom';
 import logo from "../../assets/Logo/Logo-Full-Light.png";
 import { NavbarLinks } from "../../data/navbar-links";
@@ -6,12 +6,31 @@ import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import ProfileDropDown from '../core/Homepage/Auth/ProfileDropDown';
+import { apiConnector } from '../../services/apiconnector';
+import { categories } from '../../services/apis';
+import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 const Navbar = () => {
 
     const { token } = useSelector( (state) => state.auth );
     const { user } = useSelector( (state) => state.profile );
     const { totalItems } = useSelector( (state) => state.cart );
+
+    // api calls
+    const [subLinks, setSubLinks] = useState([]);
+    const fetchSublinks = async () => {
+        try {
+            const result = await apiConnector("GET", categories.CATEGORIES_API);
+            console.log("Printing Sublinks result: ", result);
+            setSubLinks(result.data.data);
+        }
+        catch(error) {
+            console.log("Could not fetch the Category List");
+        }
+    }
+    useEffect(() => {
+        fetchSublinks();
+    }, []);
 
 
     const location = useLocation();
@@ -36,7 +55,31 @@ const Navbar = () => {
                                 {
                                     link.title === "Catalog" ? (
                                         <div>
-                                            {/*  */}
+                                            <div className='relative flex items-center gap-2 group'>
+                                                <p>{link.title}</p>
+                                                <IoIosArrowDropdownCircle />
+
+                                                <div className='invisible absolute left-[50%] translate-x-[-50%] translate-y-[80%] 
+                                                    top-[50%] flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900
+                                                    opacity-0 transition-all duration-200 group-hover:visible
+                                                    group-hover:opacity-100 lg:w-[300px]'>
+
+                                                    <div className='absolute left-[57%] top-0 translate-y-[-45%] h-6 w-6 rotate-45
+                                                     rounded bg-richblack-5'>
+                                                    </div>
+
+                                                    {
+                                                        subLinks.length ? (
+                                                                subLinks.map( (sublink, index) => (
+                                                                    <Link to={`${subLinks.link}`} key={index}>
+                                                                        <p>{sublink.title}</p>
+                                                                    </Link>
+                                                                ) )
+                                                        ) : (<div></div>)
+                                                    }
+
+                                                </div>
+                                            </div>
                                         </div>
                                     ) : (
                                         <Link to={link?.path}>
