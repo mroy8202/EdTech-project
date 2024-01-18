07789080684fe207e5d1702ad4eab361b07cdeba
mritunjayRoy <mroy8202@gmail.com>
2024-01-18 17:41:03 +0530

    SubSection issue resolved in server side

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // import controllers and middlewares
-const { capturePayment, verifySignature, sendPaymentSuccessEmail } = require("../controllers/Payments")
+const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controllers/Payments")
 const { auth, isStudent } = require("../middlewares/auth");
 
 // routes
 router.post("/capturePayment", auth, isStudent, capturePayment);
-router.post("/verifySignature", verifySignature);
+router.post("/verifyPayment", auth, isStudent, verifyPayment);
 router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
 
 // export router
