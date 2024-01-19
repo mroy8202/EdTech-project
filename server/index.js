5639d34a990628829b58d06f5e6adb9a4cafc507
mritunjayRoy <mroy8202@gmail.com>
2024-01-20 00:04:57 +0530

    cors issue resolved

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://ed-tech-project-pi.vercel.app/"],
-        credentials: true,
+      origin: ["http://localhost:3000", "https://ed-tech-project-pi.vercel.app"],
+      credentials: true,
     })
 );
 app.use(fileUpload({
