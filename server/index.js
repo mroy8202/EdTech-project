f051bdeeb30ba93a52a7d2bbf4a965e17787125c
mritunjayRoy <mroy8202@gmail.com>
2024-01-27 13:39:36 +0530

    debug

--- server/index.js
+++ server/index.js
@@ -29,6 +29,11 @@ app.use(
     })
 );
 
+app.use((req, res, next) => {
+    console.log("Request received:", req.method, req.url);
+    next();
+});
+
 app.use(fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/'
